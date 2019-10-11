e4c5b6c279ca85d9009621103ec1e78aa6523389
Kirkemann <kirkemann35@gmail.com>
2019-10-11 19:04:19 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -13,21 +13,21 @@
 var snegl1 = {
     id: "a",
     navn: "Svend",
-    foto: "images/snegl1.png",
+    foto: "assets/images/snegl1.png",
     x: -160,
     y: -80
 };
 var snegl2 = {
     id: "b",
     navn: "Aage",
-    foto: "/images/snegl2.png",
+    foto: "assets/images/snegl2.png",
     x: -160,
     y: 0
 };
 var snegl3 = {
     id: "c",
     navn: "Bente",
-    foto: "/assets/images/snegl3.png",
+    foto: "assets/images/snegl3.png",
     x: -160,
     y: 80
 };
